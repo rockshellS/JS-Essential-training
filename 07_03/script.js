70d202884f89d700e3dcf989e12e591de511db03
rockshellS <spicer.rachelle@gmail.com>
2021-04-29 16:58:29 -0500

    arrays

--- 07_03/script.js
+++ 07_03/script.js
@@ -4,17 +4,17 @@
  */
 
 let backpackContents = ["piggy", "headlamp", "pen"];
+backpackContents.forEach(function (item) {
+  item = `<li>${item}</li>`;
+  console.log(item);
+});
 
+let longItems = backpackContents.find(function (item) {
+  if (item.length >= 5) {
+    return item;
+  }
+});
+console.log("longItems:", longItems);
 console.log(backpackContents);
 
-// backpackContents.forEach(function (item) {
-//   item = `<li>${item}</li>`;
-//   console.log(item);
-// });
 
-// let longItems = backpackContents.find(function (item) {
-//   if (item.length >= 5) {
-//     return item;
-//   }
-// });
-// console.log("longItems:", longItems);
--- 07_04/script.js
+++ 07_04/script.js
@@ -1,11 +1,33 @@
 /**
  * Challenge: Build and modify an array
- * - Build an array with 8 items
- * - Remove the last item
- * - Add the last item as the first item on the array 
- * - Sort the items by alphabetical order
- * - Use the find() method to find a specific item in the array
- * - Remove the item you found using the find method from the array.
  */
 
+// * - Build an array with 8 items÷\
+let table = ["computer", "monitor", "pens", "whiteout", "cup", "notebooks", "lamp", "glasses", "glasses", "lotion"
+]
 
+
+// * - Remove the last item
+console.log(table.pop())
+
+
+
+// * - Add the last item as the first item on the array 
+table.unshift(table.pop())
+console.log(table)
+
+
+
+
+// * - Sort the items by alphabetical order
+console.log(table.sort())
+
+
+// * - Use the find() method to find a specific item in the array
+const findItem = table.find(item => item === "pens")
+console.log("Found item:",findItem)
+
+// * - Remove the item you found using the find method from the array.
+let remove = "glasses"
+table.splice(table.indexOf(remove), 4, "DOG")
+console.log(`array with "${remove}" removed:`, table)
