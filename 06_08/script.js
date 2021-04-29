bf65bcfcff2e75e38c9337e478b198130f37ecff
rockshellS <spicer.rachelle@gmail.com>
2021-04-29 12:23:30 -0500

    data

--- 06_08/script.js
+++ 06_08/script.js
@@ -3,14 +3,29 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Arithmetic_operators
  */
 
-let a = 5;
-let b = 4;
-let c = 3.2;
+// let a = 5;
+// let b = 4;
+// let c = 3.2;
 
-console.log(`let a: ${a} (${typeof a})`);
-console.log(`let b: ${b} (${typeof b})`);
-console.log(`let c: ${c} (${typeof c})`);
+// console.log(`let a: ${a} (${typeof a})`);
+// console.log(`let b: ${b} (${typeof b})`);
+// console.log(`let c: ${c} (${typeof c})`);
 
-let result = a + b;
+// let result = a * b;
 
-console.log("Result: ", result);
+// console.log("Result: ", result);
+
+var defaultColor = "purple";
+
+function setColor() {
+    if ( defaultColor === "purple" ) {
+        let defaultColor = "orange";
+    }
+}
+
+setColor();
+console.log(defaultColor);
+let sum = 23.95;
+let tip = "3";
+
+console.log("The total is $" + sum + tip + ".");
