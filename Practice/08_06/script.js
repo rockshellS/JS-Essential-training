befc420996e05ebbfa8bb0e93460bcba1812a5ed
rockshellS <spicer.rachelle@gmail.com>
2021-05-02 12:00:21 -0500

    proactice functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,20 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+function addNumbers(a,b) {
+    // const number = document.createElement('number')
+    // number.innerHTML = addNumbers()
+    return a + b 
+}
+addNumbers(1,2)
+console.log(addNumbers(5,1))
+
+const letsAddMoreNumbers = function(a,b,c)  {
+ return a + b + c 
+}
+console.log(letsAddMoreNumbers(2,3,4))
+
+const doThisOneMoreTime = (a,b,c,d) => {
+    return a + b + c + d 
+}
+console.log(doThisOneMoreTime(3,4,5,6,))
